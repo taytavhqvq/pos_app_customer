dda5b059aaf4741c4cd887e7622cf5ab341595a2
tayxtt <tayxayyalath@gmail.com>
2026-07-27 15:23:37 +0700

    fix url

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,8 +1,8 @@
 class ApiConstants {
   static const String baseUrl =
-      'http://localhost:3000/api'; //'http://10.0.2.2:3000/api';
+      'http://10.0.2.2:3000/api'; //'http://10.0.2.2:3000/api';
 
-  static const String socketUrl = 'http://localhost:3000';
+  static const String socketUrl = 'http://10.0.2.2:3000';
 
   // ===== Auth (customer) =====
   static const String login = '/customers/login';
